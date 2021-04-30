2c394219921d2bf177e65ac3116e790b5cfd00b9
Leekm0912 <42348176+Leekm0912@users.noreply.github.com>
2021-04-30 10:10:09 +0900

    데코레이터를 이용한 코드 타이머.

---
+++ CheckTime.py
@@ -0,0 +1,17 @@
+from timeit import default_timer
+from datetime import timedelta
+
+
+class CheckTime:
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        def wrapper_fn():
+            start = default_timer()
+            result = self.func(*args, **kwargs)
+            end = default_timer()
+            print("[{}]: {} sec\nresult : {}".format(self.func.__name__, timedelta(seconds=end - start), result))
+            return result
+
+        return wrapper_fn()
