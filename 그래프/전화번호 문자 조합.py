ecfa394526b262628939ec7b4c8582bf41199218
leekm0912 <skatjrdndqo@naver.com>
2021-08-05 10:29:39 +0900

    commit

--- 그래프/전화번호 문자 조합.py
+++ 그래프/전화번호 문자 조합.py
@@ -1,12 +1,17 @@
 def solution(data):
-    def dfs(index, path):
-        if len(path) == len(data):
-            answer.append(path)
+    # 매개변수 : 조합을 시작할 자리수, 만든 정답.
+    def dfs(index, answer):
+        # 만든 정답과 입력값의 길이가 같고(자리수) 기존에 없던 정답이라면 백트래킹.
+        if len(answer) == len(data) and answer not in result:
+            result.append(answer)
             return
-
+        # 정답을 만들기 위해 조합.
+        # 매개변수로 받은 시작 ~ 자리수 까지.
         for i in range(index, len(data)):
+            # 입력받은 데이터에 해당하는 문자들을 하나하나 대입
             for j in num[data[i]]:
-                dfs(i + 1, path + j)
+                # 다음 인덱스, 만든 정답에 j를 붙혀줌.
+                dfs(i + 1, answer + j)
 
     num = {
         "2": ["a", "b", "c"],
@@ -18,9 +23,9 @@ def solution(data):
         "8": ["t", "u", "v"],
         "9": ["w", "x", "y", "z"]
     }
-    answer = []
+    result = []
     dfs(0, "")
-    return answer
+    return result
 
 
 if __name__ == "__main__":
