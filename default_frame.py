a51c6bf03a10cb69dd2d347fe049335f7f421f14
leekm0912 <skatjrdndqo@naver.com>
2021-08-13 21:08:55 +0900

    commit

---
+++ default_frame.py
@@ -0,0 +1,10 @@
+import CheckTime
+
+
+@CheckTime.CheckTime
+def solution(data):
+    pass
+
+
+if __name__ == "__main__":
+    solution(None)
---
+++ 그래프/일정 재구성.py
@@ -0,0 +1,13 @@
+import CheckTime
+
+
+@CheckTime.CheckTime
+def solution(data):
+    pass
+
+
+if __name__ == "__main__":
+    solution([
+        ["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]
+    ])
+    print(sorted(["JAVA", "JSP", "Spring","Android", "Python", "Cloud", "Algorithm", "Autohotkey"]))
