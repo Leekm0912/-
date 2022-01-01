2338a471e75555a7edbb04efaeca6f919516f6a7
Leekm0912 <42348176+Leekm0912@users.noreply.github.com>
2022-01-01 22:49:49 +0900

    주석 추가

--- 그리디/분할가능배낭/Solution.java
+++ 그리디/분할가능배낭/Solution.java
@@ -8,26 +8,23 @@ public class Solution {
 		// 람다식 내부에서 쓰기위해 배열로 선언.
 		double[] answer = {0.0};
 		int[] max_weight_arr = {max_weight};
-		
+
+		// {단가, 가격, 무게} 순으로 새로 만들어 주기 위한 리스트.
 		List<double[]> pack = new ArrayList<>();
-		
-		cargo.stream().sorted((x, y)->{
-			if(x[0] > y[0]) {
-				return -1;
-			}else if(x[0] == y[0]) {
-				return 0;
-			}else{
-				return 1;
-			}
-		})
+		// {단가(가격/무게), 가격, 무게} 형식으로 저장.
+		cargo.stream()
 		.forEach(v->{
 			pack.add(new double[] {v[0] / v[1], v[0], v[1]});
 		});
-		
-		pack.stream().sorted((x, y)->{
+
+		// 단가 순으로 내림차순 정렬 후 그리디 계산.
+		// 단가가 같다면 가격 순으로 내림차순 정렬.
+		pack.stream()
+		.sorted((x, y)->{
 			if(x[0] > y[0]) {
 				return -1;
 			}else if(x[0] == y[0]) {
+				// 단가가 같다면 가격을 기준으로 내림차순으로 정렬.
 				if(x[1] > y[1]) {
 					return -1;
 				}else if(x[1] == y[1]) {
@@ -40,11 +37,16 @@ public class Solution {
 			}
 		})
 		.forEach(v->{
+			// 가방에 모두 담을수 있다면 담음.
 			if((max_weight_arr[0] - v[2]) > 0) {
 				answer[0] += v[1];
 				max_weight_arr[0] -= v[2];
-			}else {
+			}
+			// 가방에 담을수 없다면 쪼개서 담음.
+			else {
+				// 남은 무게 / 무게 로 비율을 구함.
 				double fraction = max_weight_arr[0] / v[2];
+				// 구한 비율만큼 결과에 추가.
 				answer[0] += v[1] * fraction;
 				return;
 			}
